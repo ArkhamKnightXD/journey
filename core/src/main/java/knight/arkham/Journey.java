591a5aac73908ce424c36ba774b9d4c74287b226
knight <karvin_jimenez@hotmail.com>
2023-06-25 16:57:44 -0400

    Cleaning Journey class.

--- core/src/main/java/knight/arkham/Journey.java
+++ core/src/main/java/knight/arkham/Journey.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
@@ -13,22 +12,13 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import knight.arkham.screens.GameScreen;
 
-import static knight.arkham.helpers.Constants.*;
-
 public class Journey extends Game {
     public static Journey INSTANCE;
-
     public SpriteBatch batch;
-    public BitmapFont font;
-
     public OrthographicCamera globalCamera;
-
     public Viewport viewport;
-
-    public Box2DDebugRenderer debugRenderer;
-
     public World globalWorld;
-
+    public Box2DDebugRenderer debugRenderer;
     public int screenWidth;
     public int screenHeight;
 
@@ -41,19 +31,17 @@ public class Journey extends Game {
     public void create() {
 
         globalWorld = new World(new Vector2(0, -40), true);
+        debugRenderer = new Box2DDebugRenderer();
 
         screenWidth = Gdx.graphics.getWidth();
         screenHeight = Gdx.graphics.getHeight();
 
         batch = new SpriteBatch();
-        font = new BitmapFont();
 
         globalCamera = new OrthographicCamera();
 
 //        It is better to avoid using PPM to set up my viewport.
-        viewport = new FitViewport(FULL_SCREEN_WIDTH/32f, FULL_SCREEN_HEIGHT/32f, globalCamera);
-
-        debugRenderer = new Box2DDebugRenderer();
+        viewport = new FitViewport(screenWidth/32f, screenHeight/32f, globalCamera);
 
         setScreen(new GameScreen());
     }
