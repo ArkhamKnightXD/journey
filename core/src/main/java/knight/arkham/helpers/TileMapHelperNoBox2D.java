723c6b0f6773a88ec41e3df7066ad1283701ea42
knight <karvin_jimenez@hotmail.com>
2024-07-28 08:28:31 -0400

    adapting tilemap with new collision method.

--- core/src/main/java/knight/arkham/helpers/TileMapHelperNoBox2D.java
+++ core/src/main/java/knight/arkham/helpers/TileMapHelperNoBox2D.java
@@ -52,6 +52,7 @@ public class TileMapHelperNoBox2D {
 
         for (Rectangle collision : collisionRectangles) {
 
+
             if (player.getBounds().overlaps(collision)) {
 
                 player.isPlayerGrounded = true;
